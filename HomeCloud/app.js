0050b16153903fedbe7d22b9716c8892e71db721
jakemurphy217 <jakemurphy217@gmail.com>
2019-12-17 23:53:17 +0000

    Add files via upload

--- HomeCloud/app.js
+++ HomeCloud/app.js
@@ -1,31 +1,9 @@
 
-// var express = require('express');
-// var app = express();
-// // define routes here..
-
-// app.get('/', function (req, res) {
-//     res.send('<html><body><h1>Hello World</h1></body></html>');
-// });
-// app.post('/submit-data', function (req, res) {
-//     res.send('POST Request');
-// });
-// app.put('/update-data', function (req, res) {
-//     res.send('PUT Request');
-// });
-// app.delete('/delete-data', function (req, res) {
-//     res.send('DELETE Request');
-// });
-
-
-
-// var server = app.listen(8080, function () {
-//     console.log('Node server is running at 8080 ...');
-// });
-
 var express = require('express');
 var app = express();
 var bodyParser = require("body-parser");
 
+const port = 3000
 
 
 
@@ -37,17 +15,17 @@ app.get('/', function (req, res) {
     res.sendFile('index.html',{ root: __dirname });
 
 });
-app.post('/submit-student-data', function (req, res) {
+app.post('/User-Name-Data', function (req, res) {
 
-    var name = req.body.firstName + ' ' + req.body.lastName; 
+    var name =  req.body.firstName + ' ' + req.body.lastName ; 
 
     res.send(name + ' Submitted Successfully!');
 
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(port, function () {
 
-    console.log('Node server is running..');
+    console.log('Node server is running on port' +' ' + port);
 
 });
 
